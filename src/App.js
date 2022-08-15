b22982096b82f5c4f9f2572f4507532c1244ffb8
Gaston Raices <gastonraices@gmail.com>
2022-08-15 19:40:56 -0300

    react app

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mi primera app con react.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://c.tenor.com/5ZsLvD66UdcAAAAC/jonah-hill-dance-wolf-of-wall-street-dance.gif"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go dancing
         </a>
       </header>
     </div>
