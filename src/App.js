d9849d54096aa8ef230f257f97fbd640cb1926fa
Gaston Raices <gastonraices@gmail.com>
2022-08-22 15:45:31 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Navbar } from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mi primera app con react.
-        </p>
-        <a
-          className="App-link"
-          href="https://c.tenor.com/5ZsLvD66UdcAAAAC/jonah-hill-dance-wolf-of-wall-street-dance.gif"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go dancing
-        </a>
-      </header>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import './navbar.css'
+
+
+export const Navbar = () => {
+
+    return (
+        <header className="header">
+            <div className="header-container">
+
+                <h1 className="header-title">KVOTHE</h1>
+
+                <nav className="header-navbar">
+                    <a href='' className="header-navlink">SHOP</a>
+                    <a href='' className="header-navlink">OUTLET</a>
+                    <a href='' className="header-navlink">CONTACTO</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
